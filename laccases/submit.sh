a3ee88d2ce075172096ff43ba759f792473d7aca
Mary Riley <myriley@ucdavis.edu>
2026-04-23 15:10:06 -0700

    Update submit.sh

--- laccases/submit.sh
+++ laccases/submit.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-100
 #SBATCH --partition=low
-#SBATCH --time=48:00:00
+#SBATCH --time=30:00:00
 #SBATCH --mem=10G
 
 /quobyte/jbsiegelgrp/software/Rosetta_314/rosetta/main/source/bin/rosetta_scripts.static.linuxgccrelease -database /quobyte/jbsiegelgrp/software/Rosetta_314/rosetta/main/database @flags -user_tag $SLURM_ARRAY_TASK_ID -out:suffix $SLURM_ARRAY_TASK_ID -out:path:all ./results
